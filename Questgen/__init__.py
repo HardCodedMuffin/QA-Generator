219c75b2b7f59b35bf210f2c63685fe53e65c27b
Ömer Çelik <s170444022@stu.thk.edu.tr>
2021-03-31 16:08:42 +0300

    Init update

--- Questgen/__init__.py
+++ Questgen/__init__.py
@@ -2,4 +2,4 @@
 from __future__ import absolute_import
 from Questgen.encoding import encoding
 from Questgen.mcq import mcq
-from Questgen.main import QGen, BoolQGen, AnswerPredictor
+from Questgen.main import QGen, AnswerPredictor
