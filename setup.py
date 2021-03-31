d463b1297713ab2b7e492d7bff6173a38c8b8b18
Ömer Çelik <s170444022@stu.thk.edu.tr>
2021-03-31 17:00:09 +0300

    Setup Update

--- setup.py
+++ setup.py
@@ -1,31 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='Questgen',
       version='1.0.0',
       description='Question generator from any text',
       packages=['Questgen', 'Questgen.encoding', 'Questgen.mcq'],
       url="https://github.com/mr-CLK/QA-Generator.git",
-      install_requires=[
-
-          'torch==1.8.1',
-          'transformers==4.4.2',
-          'pytorch_lightning==0.8.1',
-          'strsim==0.0.3',
-          'six==1.15.0',
-          'networkx==2.4.0',
-          'numpy',
-          'scipy',
-          'scikit-learn',
-          'unidecode==1.1.1',
-          'future==0.18.2',
-          'joblib==0.14.1',
-          'spacy==2.2.4',
-          'pytz==2020.1',
-          'python-dateutil==2.8.1',
-          'boto3==1.14.40',
-          'flashtext==2.7',
-          'pke~=1.8.1',
-          'nltk~=3.5'
-      ],
+      install_requires=required,
       package_data={'Questgen': ['questgen.py', 'mcq.py', 'train_gpu.py', 'encoding.py']}
       )
